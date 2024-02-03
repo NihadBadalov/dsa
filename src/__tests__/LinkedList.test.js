af9a0d20c8cedfda1faf7dee307341deb4d4b713
NihadBadalov <32594553+NihadBadalov@users.noreply.github.com>
2024-02-03 21:15:00 +0100

    add tests and minor bugfix

---
+++ src/__tests__/LinkedList.test.js
@@ -0,0 +1,120 @@
+const LinkedList = require("../dsa/LinkedList.cjs");
+
+test('append', () => {
+  const ll = new LinkedList([1, 2, 3, 4]);
+  ll.append(5);
+  expect([...ll].map(node => node.value)).toStrictEqual([1, 2, 3, 4, 5]);
+});
+
+test('prepend', () => {
+  const ll = new LinkedList([1, 2, 3, 4]);
+  ll.prepend(5);
+  expect([...ll].map(node => node.value)).toStrictEqual([5, 1, 2, 3, 4]);
+});
+
+test('insert', () => {
+  const ll = new LinkedList([1, 2, 3, 4]);
+  ll.insert(5, 2);
+  expect([...ll].map(node => node.value)).toStrictEqual([1, 2, 5, 3, 4]);
+});
+
+test('remove - from start', () => {
+  const ll = new LinkedList([1, 2, 3, 4]);
+  ll.remove(0);
+  expect([...ll].map(node => node.value)).toStrictEqual([2, 3, 4]);
+});
+
+test('remove - from middle', () => {
+  const ll = new LinkedList([1, 2, 3, 4]);
+  ll.remove(1);
+  expect([...ll].map(node => node.value)).toStrictEqual([1, 3, 4]);
+});
+
+test('remove - from end', () => {
+  const ll = new LinkedList([1, 2, 3, 4]);
+  ll.remove(3);
+  expect([...ll].map(node => node.value)).toStrictEqual([1, 2, 3]);
+});
+
+test('indexOf - normaly', () => {
+  const ll = new LinkedList([1, 2, 3, 4]);
+  expect(ll.indexOf(3)).toBe(2);
+});
+
+test('indexOf - element not in Linked List', () => {
+  const ll = new LinkedList([1, 2, 3, 4]);
+  expect(ll.indexOf(7)).toBe(-1);
+});
+
+test('indexOf - Linked List is empty', () => {
+  const ll = new LinkedList([]);
+  expect(ll.indexOf(0)).toBe(-1);
+});
+
+test('at - linear search from both left and right', () => {
+  const ll = new LinkedList([1, 2, 3, 4]);
+  expect(ll.at(1).value + ll.at(2).value).toBe(5);
+});
+
+test('at - linear search from left', () => {
+  const ll = new LinkedList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  expect(ll.at(2).value).toBe(3);
+});
+
+test('at - linear search from right', () => {
+  const ll = new LinkedList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  expect(ll.at(8).value).toBe(9);
+});
+
+test('sort', () => {
+  const ll = new LinkedList([-2, 2, -3, 1, 2, 0, 1, 1337, Infinity, -Infinity]);
+  ll.sort();
+
+  expect(
+    [...ll].map(node => node.value)
+  ).toStrictEqual([
+    -Infinity, -3, -2,
+    0, 1, 1, 2, 2,
+    1337, Infinity,
+  ]);
+});
+
+test('shuffle', () => {
+  const ll = new LinkedList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  ll.shuffle();
+  expect([...ll].map(node => node.value)).not.toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+});
+
+test('reverse and reverseGenerator', () => {
+  const ll1 = new LinkedList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  let ll2 = new LinkedList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  ll2 = [...ll2.reverseGenerator()].reverse();
+
+  expect([...ll2].map(node => node.value)).toStrictEqual([...ll1].map(node => node.value));
+});
+
+test('random', () => {
+  const ll = new LinkedList([1337, 1338, 1339, 1440, 1441, 1442]);
+  const random = ll.random().value;
+  expect([...ll].map(node => node.value)).toContain(random);
+});
+
+
+test('cycle', () => {
+  const ll = new LinkedList([1, 2, 3]);
+  const elements = [];
+
+  let i = 0;
+  for (const e of ll.cycle()) {
+    if (i >= 9) break;
+    elements.push(e);
+    i++;
+  }
+
+  expect(elements.map(node => node.value)).toStrictEqual([1, 2, 3, 1, 2, 3, 1, 2, 3]);
+});
+
+test('toString', () => {
+  const ll = new LinkedList([1, 2, 3]);
+  expect(ll.toString()).toMatch(/1, 2, 3/);
+});
---
+++ src/dsa/LinkedList.cjs
@@ -0,0 +1,288 @@
+class Node {
+  value = undefined;
+  next = undefined;
+  previous = undefined;
+
+  /**
+   * @param {any} value 
+   * @param {Node | undefined} next 
+   * @param {Node | undefined} previous 
+   */
+  constructor(value, next, previous) {
+    this.value = value;
+    this.next = next;
+    this.previous = previous;
+  }
+}
+
+class LinkedList {
+  head = undefined;
+  length = 0;
+
+  constructor(iterator) {
+    if (!iterator) return;
+
+    try {
+      for (const e of iterator) {
+        this.append(e);
+      }
+    } catch (e) { }
+  }
+
+  /**
+    * Adds an element to the end of Linked List
+   * @param {any} e
+   */
+  append(e) {
+    if (!this.head) {
+      const node = new Node(e);
+      node.next = node;
+      node.previous = node;
+      this.head = node;
+      this.length = 1;
+      return;
+    }
+
+    const node = new Node(e, this.head, this.head.previous);
+    this.head.previous.next = node;
+    this.head.previous = node;
+    this.length++;
+  }
+
+  /**
+    * Same thing as append(...)
+   * @param {any} e
+   */
+  push(e) { append(e) }
+
+  /**
+    * Adds element to the beginning of Linked List
+   * @param {any} e 
+   */
+  prepend(e) {
+    if (!this.head) {
+      const node = new Node(e);
+      node.next = node;
+      node.previous = node;
+      this.head = node;
+      this.length = 1;
+      return;
+    }
+
+    const node = new Node(e, this.head, this.head.previous);
+    this.head.previous.next = node;
+    this.head.previous = node;
+    this.head = node;
+    this.length++;
+  }
+
+  /**
+    * Insert a value at index to Linked List
+   * @param {any} val Value to insert
+   * @param {number} ind Index to insert the value at
+   */
+  insert(val, i) {
+    const ind = i >= 0 ? i % this.length : this.length - (-i % this.length);
+
+    if (ind === 0) {
+      return this.prepend(val);
+    }
+    if (ind === this.length) {
+      return this.append(val);
+    }
+
+    const e = this.at(ind);
+    if (!e) return this.append(val);
+
+    const node = new Node(val, e, e.previous);
+    e.previous.next = node;
+    e.previous = node;
+    this.length++;
+  }
+
+  /**
+    * Removes element with index of x
+   * @param {number} x Index of x to remove
+   */
+  remove(i) {
+    /** @type {Node | undefined} */
+    const e = this.at(i);
+    if (!e) return undefined;
+
+    if (this.length === 1) {
+      this.head = undefined;
+      this.length = 0;
+      return;
+    }
+
+    e.previous.next = e.next;
+    e.next.previous = e.previous;
+    this.length--;
+
+    if (i === 0) {
+      this.head = this.head.next === this.head ? undefined : this.head.next;
+    }
+  }
+
+  /**
+   * @param {any} val Value to search index of
+   * @returns {number} If found, element of index; otherwise, -1
+   */
+  indexOf(val) {
+    let i = 0;
+    for (const e of this) {
+      if (val === e.value) return i;
+      i++;
+    }
+    return -1;
+  }
+
+  /**
+    * Returns element at index n
+   * @param {number} n Index
+   */
+  at(n) {
+    const ind = n >= 0 ? n % this.length : this.length - (-n % this.length);
+    let i = this.length < 4 ? 0 : (ind < Math.floor(this.length / 2) ? 0 : this.length - 1);
+    const side = i === 0 ? 0 : 1;
+    /*
+      * Removed as it uses more memory than needed
+      *
+      * for (const e of (i === 0 ? this : this.reversedIterator())) {
+      if (i === ind) return e;
+      if (side === 0) i++;
+      else i--;
+    } */
+
+    let current = side === 0 ? this.head : this.head.previous;
+    while (current) {
+      if (i === ind) return current;
+      current = side === 0
+        ? current.next
+        : current.previous;
+      if (side === 0) i++;
+      else i--;
+    }
+
+    return undefined;
+  }
+
+  /** Sort Linked List in place */
+  sort() {
+    // future Me, sorry. can't be bothered to normally implement this right now
+    // at least this is either O(n log n) or O(n) - (not sure what the arr.sort T.Compl. is)
+    let elements = [];
+    for (const e of this) {
+      elements.push(e);
+    }
+    elements = elements.sort((a, b) => a.value - b.value);
+
+    while (this.length > 0) {
+      this.remove(0);
+    }
+
+    for (const e of elements) {
+      this.append(e.value);
+    }
+  }
+
+  /** Shuffle Linked List in place */
+  shuffle() {
+    // future Me, sorry. can't be bothered to normally implement this right now
+    // at least this is either O(n log n) or O(n) - (not sure what the arr.sort T.Compl. is)
+    let elements = [];
+    for (const e of this) {
+      elements.push(e);
+    }
+    elements = ((array) => {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
+    })(elements);
+
+    while (this.length > 0) {
+      this.remove(0);
+    }
+
+    for (const e of elements) {
+      this.append(e.value);
+    }
+  }
+
+  /** Reverses Linked List in place */
+  reverse() {
+    let originalHead = this.head;
+    let current = this.head;
+    let previous = current?.previous;
+
+    while (current) {
+      const next = current.next;
+      current.previous = next;
+      current.next = previous;
+      current = next;
+      previous = next.previous;
+      if (current === originalHead) break;
+    }
+
+    this.head = this.head.next;
+  }
+
+  /**
+    * Return a random element of Linked List
+   * @returns {Node | undefined}
+   */
+  random() {
+    if (!this.length) return undefined;
+    const randomIdx = Math.floor(Math.random() * this.length);
+    return this.at(randomIdx);
+  }
+
+  /** @returns {Node | undefined} */
+  *[Symbol.iterator]() {
+    let current = this.head;
+    while (current) {
+      yield current;
+      current = current.next;
+      if (current === this.head) return;
+    }
+  }
+
+  /** Cycle through Linked List */
+  *reverseGenerator() {
+    let current = this.head.previous;
+    while (current) {
+      yield current;
+      current = current.previous;
+      if (current.next === this.head) return;
+    }
+  }
+
+  /** Cycle through Linked List */
+  *cycle() {
+    let current = this.head;
+    while (current) {
+      yield current;
+      current = current.next;
+    }
+  }
+
+  /** Cycle through Linked List */
+  *shuffleCycle() {
+    while (true) {
+      const randomIdx = Math.floor(Math.random() * this.length);
+      yield this.at(randomIdx);
+    }
+  }
+
+  toString() {
+    const elements = [];
+    for (const e of this) {
+      elements.push(typeof e.value === 'string' ? `'${e.value}'` : e.value);
+    }
+    return elements.join(', ');
+  }
+}
+
+module.exports = LinkedList;
